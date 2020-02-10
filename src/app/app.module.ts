64be1662c66a2035d2cb092b9e9cb459a0fc782c
sumela1988 <sumela.mixa@gmail.com>
2020-02-10 12:05:44 +0100

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
@@ -19,7 +19,6 @@ import {Location, CommonModule} from '@angular/common';
   imports: [
     BrowserModule,
     HttpClientModule,
-    HttpModule,
     CommonModule,
     ReactiveFormsModule,
     RouterModule.forRoot([      
