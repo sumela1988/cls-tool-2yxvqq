205412020a27d112fba84b5241421aa25dd8dab7
sumela1988 <sumela.mixa@gmail.com>
2020-02-10 12:27:19 +0100

    2 files modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Product } from "../products";
 import { BehaviorSubject, Observable, Subject, Subscriber } from "rxjs";
-import { of } from "rxjs/observable/of";
+
 
 @Injectable()
 export class CartService {
