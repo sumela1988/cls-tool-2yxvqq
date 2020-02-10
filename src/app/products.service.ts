8bc646c2996a7341b907fceeed14c28d60e00954
sumela1988 <sumela.mixa@gmail.com>
2020-02-10 12:38:17 +0100

    1 file modified

--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Product } from '../products';
 import { Observable } from 'rxjs';
-import { of } from 'rxjs/observable/of';
 
 @Injectable()
 export class ProductsService {
