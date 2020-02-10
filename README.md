b37ab47c2c29c206c5df09599f1c0ef4c6da2084
sumela1988 <sumela.mixa@gmail.com>
2020-02-10 08:26:17 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# cls-tool-2yxvqq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/cls-tool-2yxvqq)
---
+++ src/app/products.ts
@@ -0,0 +1,224 @@
+export const products = [
+  {
+        name: 'AH Komkommer',
+        price: 0.70,
+        unit: 'per stuk'
+    },
+    {
+        name: 'AH Mandarijnen net',
+        price: 1.89,
+        unit: '1 kg'
+    },
+    {
+        name: 'AH Bananen',
+        price: 1.19,
+        unit: 'Tros'
+    },
+    {
+        name: 'Chiquita Bananen',
+        price: 1.99,
+        unit: '1 kg'
+    },
+    {
+        name: 'AH Courgette',
+        price: 0.66,
+        unit: 'per stuk'
+    },
+    {
+        name: 'AH Snoepgroente tomaat',
+        price: 2.99,
+        unit: '500 g'
+    },
+    {
+        name: 'AH Broccoli',
+        price: 1.19,
+        unit: '500 g'
+    },
+    {
+        name: 'AH Rode paprika',
+        price: 0.79,
+        unit: 'per stuk'
+    },
+    {
+        name: 'AH Paprika mix',
+        price: 1.60,
+        unit: '3 stuks'
+    },
+    {
+        name: 'AH Avocado eetrijp',
+        price: 2.79,
+        unit: '2 stuks'
+    },
+    {
+        name: 'AH Pitloze witte druiven',
+        price: 2.19,
+        unit: '500 g'
+    },
+    {
+        name: 'AH Cherry tomaten',
+        price: 0.90,
+        unit: '250 g'
+    },
+    {
+        name: 'AH Geschrapte worteltjes voordeel',
+        price: 1.59,
+        unit: '500 g'
+    },
+    {
+        name: 'AH Zoete kleine appeltjes',
+        price: 2.89,
+        unit: '1 kg'
+    },
+    {
+        name: 'Pink Lady Appelen',
+        price: 2.69,
+        unit: '4 stuks'
+    },
+    {
+        name: 'AH Tomaten',
+        price: 1.09,
+        unit: '500 g'
+    },
+    {
+        name: 'AH Stamppot aardappelen',
+        price: 1.19,
+        unit: '600 g'
+    },
+    {
+        name: 'AH Elstar',
+        price: 1.99,
+        unit: '1,5 kg'
+    },
+    {
+        name: 'AH Champignons fijn',
+        price: 0.95,
+        unit: '250 g'
+    },
+    {
+        name: 'AH Pitloze rode druiven',
+        price: 1.79,
+        unit: '500 g'
+    },
+    {
+        name: 'AH Zoete rode puntpaprikas',
+        price: 1.99,
+        unit: '2 stuks'
+    },
+    {
+        name: 'AH Boerenkool gesneden',
+        price: 1.29,
+        unit: '300 g'
+    },
+    {
+        name: 'AH Conference peren',
+        price: 1.89,
+        unit: '1 kg'
+    },
+    {
+        name: 'AH Kilotje iets kruimige aardappelen',
+        price: 1.79,
+        unit: '1 kg'
+    },
+    {
+        name: 'AH Prei',
+        price: 0.50,
+        unit: 'per stuk'
+    },
+    {
+        name: 'AH Spruiten',
+        price: 1.19,
+        unit: '500 g'
+    },
+    {
+        name: 'AH Perssinaasappelen',
+        price: 2.99,
+        unit: '2 kg'
+    },
+    {
+        name: 'Chiquita Bananen',
+        price: 2.29,
+        unit: '1,25 kg'
+    },
+    {
+        name: 'AH  Roerbakgroente Italiaans',
+        price: 1.99,
+        unit: '400 g'
+    },
+    {
+        name: 'AH Middelgrote uien',
+        price: 1.09,
+        unit: '1 kg'
+    },
+    {
+        name: 'AH Witlof',
+        price: 1.49,
+        unit: '4 stuks'
+    },
+    {
+        name: 'AH Avocado',
+        price: 1.29,
+        unit: 'per stuk'
+    },
+    {
+        name: 'AH Handsinaasappelen medium',
+        price: 2.99,
+        unit: '2 kg'
+    },
+    {
+        name: 'AH Bloemkool',
+        price: 2.00,
+        unit: 'per stuk'
+    },
+    {
+        name: 'AH Spinazie',
+        price: 1.59,
+        unit: '400 g'
+    },
+    {
+        name: 'AH Kiwi groen',
+        price: 2.89,
+        unit: '1 kg'
+    },
+    {
+        name: 'Pink Lady Appelen',
+        price: 2.69,
+        unit: '4 stuks'
+    },
+    {
+        name: 'AH Blauwe bessen',
+        price: 2.45,
+        unit: '150 g'
+    },
+    {
+        name: 'AH Nederlandse aardbeien',
+        price: 5.99,
+        unit: '500 g'
+    },
+    {
+        name: 'AH Mango eetrijp',
+        price: 2.99,
+        unit: '2 stuks'
+    },
+    {
+        name: 'AH Ovengroente zoete aardappel, rode biet',
+        price: 3.49,
+        unit: '600 g'
+    },
+    {
+        name: 'AH Ovengroente spruitjes pompoen rode ui',
+        price: 3.49,
+        unit: '600 g'
+    },
+    {
+        name: 'AH Ovengroente courgette rode paprika peen',
+        price: 3.49,
+        unit: '600 g'
+    }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
